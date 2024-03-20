098029640377de99e86b1b3351bda5cbb66c38d7
mohammedasimahmed <asim60999@gmail.com>
2024-03-21 02:56:28 +0530

    url changes

--- utils/socketInitialize.js
+++ utils/socketInitialize.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const socketInitialize = () => {
-  const socket = io("http://localhost:5000");
+  const socket = io("https://real-time-drawing-board-backend.onrender.com");
   return socket;
 };
 
